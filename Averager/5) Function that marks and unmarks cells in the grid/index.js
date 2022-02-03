36c306282b95ab3bc317d572e675f07cce6d80fe
sophiesundqvist <sophie.sundqvist@hotmail.com>
2022-02-03 13:08:04 +0100

    Part5; Added an event-listener that gave created divs a classList.toggle

--- Averager/5) Function that marks and unmarks cells in the grid/index.js
+++ Averager/5) Function that marks and unmarks cells in the grid/index.js
@@ -52,3 +52,44 @@ the classList.
 */
 
 
+
+
+
+let valueColumn = document.querySelector("#inputCols").value;
+let valueRow = document.querySelector ("#inputRows").value;
+
+
+function gridMaker(gridContainer, C, R){
+        
+        gridContainer.style.display = "grid";
+        gridContainer.style["grid-template-columns"] = `repeat(${C}, 1fr`;
+        gridContainer.style["grid-template-rows"] = `repeat(${R}, 1fr`;
+        
+        for(let i = 0; i < C * R; i++){
+                let createdDivs = createNumberDiv()
+                gridContainer.appendChild(createdDivs)
+
+                createdDivs.addEventListener("click",function(){
+                    createdDivs.classList.toggle("selected")
+                })
+            }
+        };
+        
+        // I Loopen kallar jag på nedan function. ger den ett nytt namn och placerar den under gridcontainer
+        
+        console.log(gridMaker(document.querySelector("#grid"), valueColumn, valueRow) )
+        
+        // har skapat en function som skapara nya divar och lägger till en siffra mellan 0-99 i
+        
+        function createNumberDiv (){
+            let newDiv = document.createElement("div")
+            newDiv.innerHTML= Math.floor(99 * Math.random())
+    
+        return newDiv
+}
+
+document.getElementsByClassName (".selected").addEventListener("click", function(){
+
+
+})
+
